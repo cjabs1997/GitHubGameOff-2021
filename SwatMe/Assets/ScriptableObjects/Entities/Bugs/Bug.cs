146d01d235a8f4efa631a746252882e53a3ccd7a
Ryan Mauldin <ryant.mauldin@gmail.com>
2021-11-23 19:00:15 -0800

    Fly art

--- SwatMe/Assets/ScriptableObjects/Entities/Bugs/Bug.cs
+++ SwatMe/Assets/ScriptableObjects/Entities/Bugs/Bug.cs
@@ -6,6 +6,12 @@ public class Bug : ScriptableObject
     public float health = 1f;
     public void onHit(){ 
         health -= 1;
+        if(health <= 0){
+            this.handleDeath();
+        }
+    }
+    private void handleDeath(){
+        Destroy(this);
     }
     public BugBehavior behavior;
 }
